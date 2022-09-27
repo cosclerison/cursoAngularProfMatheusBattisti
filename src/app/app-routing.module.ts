b467e5808a717165c20dd6a13d78b8b09f38645e
Clerison <cosclerison@gmail.com>
2022-09-27 13:53:14 -0300

    Rotas Dinamicas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
+import { FirstComponentComponent } from './components/first-component/first-component.component';
 import { TwoWayBindingComponent } from './components/two-way-binding/two-way-binding.component';
-import { PipesComponent } from './components/pipes/pipes.component';
+import { ChangeNumberComponent } from './components/change-number/change-number.component';
+import { ItemDetailComponent } from './components/item-detail/item-detail.component';
 import { ParentDataComponent } from './components/parent-data/parent-data.component';
+import { DirectivesComponent } from './components/directives/directives.component';
+import { ListRenderComponent } from './components/list-render/list-render.component';
 import { IfRenderComponent } from './components/if-render/if-render.component';
 import { EmitterComponent } from './components/emitter/emitter.component';
-import { DirectivesComponent } from './components/directives/directives.component';
-import { ChangeNumberComponent } from './components/change-number/change-number.component';
-import { ListRenderComponent } from './components/list-render/list-render.component';
-import { FirstComponentComponent } from './components/first-component/first-component.component';
+import { PipesComponent } from './components/pipes/pipes.component';
 
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
@@ -14,7 +15,8 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {path: '', component: FirstComponentComponent},
   {path: 'list', component: ListRenderComponent},
-  {path: 'change-number', component: ChangeNumberComponent},
+  {path: 'list/:id', component: ItemDetailComponent},
+  {path: 'change-number', component: ChangeNumberComponent},  
   {path: 'directives', component: DirectivesComponent},
   {path: 'emitter', component: EmitterComponent},
   {path: 'if-render', component: IfRenderComponent},
--- src/app/services/list.service.ts
+++ src/app/services/list.service.ts
@@ -21,4 +21,9 @@ export class ListService {
   getAll(): Observable<Animal[]> {
     return this.http.get<Animal[]>(this.apiUrl)
   }
+
+  getItem(id: Number): Observable<Animal> {
+    return this.http.get<Animal>(`${this.apiUrl}/${id}`);
+
+  }
 }
